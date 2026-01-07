aeabd3044bdb6d9f3ca3578f4ee0a6c024e3ca22
kangminjong <minjong2237@gmail.com>
2026-01-07 13:33:12 +0900

    feat: calculate_average

--- exercise.py
+++ exercise.py
@@ -44,7 +44,7 @@ def calculate_average(numbers: list) -> float:
         15.0
     """
     # TODO: 리스트의 평균을 계산하여 반환하는 코드를 작성하세요
-
+    return sum(numbers) / len(numbers)
 
 
 def find_max(numbers: list) -> int:
